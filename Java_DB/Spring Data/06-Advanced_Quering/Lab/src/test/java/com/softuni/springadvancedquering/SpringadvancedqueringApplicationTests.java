fab7b6cacb69a3b28aa247d8a4d173875441b2c2
Rumen Rashev <57558028+rumenrashev@users.noreply.github.com>
2020-11-12 11:25:40 +0200

    add solotion

---
+++ Java_DB/Spring Data/06-Advanced_Quering/Lab/src/test/java/com/softuni/springadvancedquering/SpringadvancedqueringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.springadvancedquering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringadvancedqueringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
